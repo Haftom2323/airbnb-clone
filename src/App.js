27694420d82d65a15c6fc1c0eed041cb1b8aef6d
Haftom2323 <haftomtek23@gmail.com>
2024-11-25 14:17:55 +0300

    airbnb clone final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Footer from "./Footer";
+import Header from "./Header";
+import Home from "./Home";
+import SearchPage from "./SearchPage";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Header />
+    
+
+        <Routes>
+          <Route path="/search" element={<SearchPage />} />
+          <Route path="/" element={<Home />} />
+        </Routes>
+
+        <Footer />
+      </Router>
+      {/* Home */}
+      {/* Header */}
+
+      {/* Banner */}
+      {/* Search */}
+
+      {/* Cards */}
+
+      {/* Footer */}
+      {/* Search page */}
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBnsU5zm_S2puQNuvBJ3ZJtpqS1DeRKck8",
+    authDomain: "facebook-cl-2413b.firebaseapp.com",
+    projectId: "facebook-cl-2413b",
+    storageBucket: "facebook-cl-2413b.firebasestorage.app",
+    messagingSenderId: "321162382099",
+    appId: "1:321162382099:web:5f3518baa417cd1ef8e8e6"
+  };
